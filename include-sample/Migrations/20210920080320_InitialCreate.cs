71f443ebb8c838f0c79a2e592a8ffa155e079dc2
naoki-funawatari <g011c1180@gmail.com>
2021-09-20 17:39:39 +0900

    feat: マイグレーション。

--- include-sample/Migrations/20210920080320_InitialCreate.cs
+++ include-sample/Migrations/20210920080320_InitialCreate.cs
@@ -20,7 +20,7 @@ namespace include_sample.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "second_layers",
+                name: "second_layers_a",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
@@ -29,9 +29,9 @@ namespace include_sample.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_second_layers", x => x.Id);
+                    table.PrimaryKey("PK_second_layers_a", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_second_layers_first_layers_FirstLayerId",
+                        name: "FK_second_layers_a_first_layers_FirstLayerId",
                         column: x => x.FirstLayerId,
                         principalTable: "first_layers",
                         principalColumn: "Id",
@@ -39,69 +39,96 @@ namespace include_sample.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "third_layers",
+                name: "second_layers_b",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    SecondLayerId = table.Column<int>(type: "integer", nullable: false)
+                    FirstLayerId = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_third_layers", x => x.Id);
+                    table.PrimaryKey("PK_second_layers_b", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_third_layers_second_layers_SecondLayerId",
-                        column: x => x.SecondLayerId,
-                        principalTable: "second_layers",
+                        name: "FK_second_layers_b_first_layers_FirstLayerId",
+                        column: x => x.FirstLayerId,
+                        principalTable: "first_layers",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
-                name: "fourth_layers",
+                name: "third_layers_a",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ThirdLayerId = table.Column<int>(type: "integer", nullable: false)
+                    SecondLayerAId = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_fourth_layers", x => x.Id);
+                    table.PrimaryKey("PK_third_layers_a", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_fourth_layers_third_layers_ThirdLayerId",
-                        column: x => x.ThirdLayerId,
-                        principalTable: "third_layers",
+                        name: "FK_third_layers_a_second_layers_a_SecondLayerAId",
+                        column: x => x.SecondLayerAId,
+                        principalTable: "second_layers_a",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "third_layers_b",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SecondLayerBId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_third_layers_b", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_third_layers_b_second_layers_b_SecondLayerBId",
+                        column: x => x.SecondLayerBId,
+                        principalTable: "second_layers_b",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_fourth_layers_ThirdLayerId",
-                table: "fourth_layers",
-                column: "ThirdLayerId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_second_layers_FirstLayerId",
-                table: "second_layers",
+                name: "IX_second_layers_a_FirstLayerId",
+                table: "second_layers_a",
                 column: "FirstLayerId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_third_layers_SecondLayerId",
-                table: "third_layers",
-                column: "SecondLayerId");
+                name: "IX_second_layers_b_FirstLayerId",
+                table: "second_layers_b",
+                column: "FirstLayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_third_layers_a_SecondLayerAId",
+                table: "third_layers_a",
+                column: "SecondLayerAId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_third_layers_b_SecondLayerBId",
+                table: "third_layers_b",
+                column: "SecondLayerBId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "fourth_layers");
+                name: "third_layers_a");
 
             migrationBuilder.DropTable(
-                name: "third_layers");
+                name: "third_layers_b");
 
             migrationBuilder.DropTable(
-                name: "second_layers");
+                name: "second_layers_a");
+
+            migrationBuilder.DropTable(
+                name: "second_layers_b");
 
             migrationBuilder.DropTable(
                 name: "first_layers");
