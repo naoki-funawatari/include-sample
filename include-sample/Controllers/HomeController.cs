1169c955ec738371c1dc989253c5141aab7b4e58
naoki-funawatari <g011c1180@gmail.com>
2021-09-20 15:24:37 +0900

    feat: サンプルデータを登録。

--- include-sample/Controllers/HomeController.cs
+++ include-sample/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using include_sample.Models;
+using JuniorTennis.Infrastructure.DataBase;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,10 +12,14 @@ namespace include_sample.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SampleDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            SampleDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +36,55 @@ namespace include_sample.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("/CreateInitialData")]
+        public async Task<IActionResult> CreateInitialData()
+        {
+            // 1層目登録
+            var firstLayers = Enumerable.Range(1, 10000)
+                .Select(_ => new FirstLayer())
+                .ToList();
+            await _context.FirstLayers.AddRangeAsync(firstLayers);
+            await _context.SaveChangesAsync();
+
+            // 2層目登録
+            firstLayers = await _context.FirstLayers.ToListAsync();
+            firstLayers.ForEach(async firstLayer =>
+            {
+                firstLayer.SecondLayers = Enumerable.Range(1, 10)
+                    .Select(_ => new SecondLayer())
+                    .ToList();
+
+                await _context.SecondLayers.AddRangeAsync(firstLayer.SecondLayers);
+            });
+            await _context.SaveChangesAsync();
+
+            // 3層目登録
+            var secondLayers = await _context.SecondLayers.ToListAsync();
+            secondLayers.ForEach(async secondLayer =>
+            {
+                secondLayer.ThirdLayers = Enumerable.Range(1, 10)
+                .Select(_ => new ThirdLayer())
+                .ToList();
+
+                await _context.ThirdLayers.AddRangeAsync(secondLayer.ThirdLayers);
+            });
+            await _context.SaveChangesAsync();
+
+            // 4層目登録
+            var thirdLayers = await _context.ThirdLayers.ToListAsync();
+            thirdLayers.ForEach(async thirdLayer =>
+            {
+                thirdLayer.FourthLayers = Enumerable.Range(1, 10)
+                    .Select(_ => new FourthLayer())
+                    .ToList();
+
+                await _context.FourthLayers.AddRangeAsync(thirdLayer.FourthLayers);
+            });
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
