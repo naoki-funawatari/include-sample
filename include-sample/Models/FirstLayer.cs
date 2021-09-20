423f8791bdc7ba5879dae8eb33c375adcb879627
naoki-funawatari <g011c1180@gmail.com>
2021-09-20 13:33:16 +0900

    feat: モデルを作成。

---
+++ include-sample/Models/FirstLayer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace include_sample.Models
+{
+    public class FirstLayer : LayerBase
+    {
+        public List<LayerBase> Values { get; set; }
+    }
+}
---
+++ include-sample/Models/ThirdLayer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace include_sample.Models
+{
+    public class ThirdLayer : LayerBase
+    {
+        public List<LayerBase> Values { get; set; }
+    }
+}
